06cf0a7b7a0fe6566891dbb9e2912534ef85fa93
John Kristan Torremocha <johnkristan01@gmail.com>
2025-07-13 11:14:49 +0800

    feat: update and delete chat

--- repository/chats.py
+++ repository/chats.py
@@ -2,6 +2,7 @@ from sqlalchemy import select
 from sqlalchemy.orm import Session
 from models.chats import Chats
 from schema.chats import ChatCreate, ChatsOut
+from fastapi import HTTPException, status
 
 class ChatsRepository:
     def __init__(self, db: Session):
@@ -18,3 +19,22 @@ class ChatsRepository:
         stmt = select(Chats.id, Chats.title)
         results = self.db.execute(stmt).all()
         return [{"id": row.id, "title": row.title} for row in results]
+    
+    
+    def update_chat_title(self, chat_id: int, new_title: str) -> Chats:
+        chat = self.db.get(Chats, chat_id)
+        if not chat:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Chat not found")
+        chat.title = new_title
+        self.db.commit()
+        self.db.refresh(chat)
+        return chat.id
+    
+    
+    def delete_chat(self, chat_id: int) -> bool:
+        chat = self.db.get(Chats, chat_id)
+        if not chat:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Chat not found")
+        self.db.delete(chat)
+        self.db.commit()
+        return True
