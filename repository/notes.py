a2d38aaf8dcc3c9a94a3a66a34e72b1ceea7e81b
John Kristan Torremocha <johnkristan01@gmail.com>
2025-07-14 13:24:45 +0800

    feat: note CRUD repository

---
+++ repository/notes.py
@@ -0,0 +1,44 @@
+from typing import List
+from sqlalchemy.orm import Session
+from models.notes import Notes
+from schema.notes import NotesCreate, NotesOut
+from fastapi import HTTPException, status
+
+class NotesRepository:
+    def __init__(self, db: Session):
+        self.db = db
+        
+    def create_note(self, note_data: NotesCreate) -> Notes:
+        new_note = Notes(**note_data.model_dump())
+        self.db.add(new_note)
+        self.db.commit()
+        self.db.refresh(new_note)
+        return new_note.id
+    
+    
+    
+    def get_all_notes(self) -> List[NotesOut]:
+        return (
+            self.db.query(Notes)
+            .order_by(Notes.created_at.desc()) 
+            .all()
+        )
+        
+        
+    def update_note_content(self, note_id: int, update_content: str) -> Notes:
+        note = self.db.get(Notes, note_id)
+        if not note:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Note not found")
+        note.content = update_content
+        self.db.commit()
+        self.db.refresh(note)
+        return note.id
+    
+    
+    def delete_note(self, note_id: int) -> bool:
+        note = self.db.get(Notes, note_id)
+        if not note:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Note not found")
+        self.db.delete(note)
+        self.db.commit()
+        return note.id
