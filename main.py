7a492df6ff35ef3b389a98c172558bc5834907e7
John Kristan Torremocha <johnkristan01@gmail.com>
2025-07-14 13:25:33 +0800

    include notes router

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.v1.chats import router as chats_router
 from api.v1.messages import router as messages_router
+from api.v1.notes import router as notes_router
 
 # Setup FastAPI app
 app = FastAPI()
@@ -16,3 +17,4 @@ app.add_middleware(
 
 app.include_router(chats_router, prefix="/api/v1", tags=["chat"])
 app.include_router(messages_router, prefix="/api/v1", tags=["messages"])
+app.include_router(notes_router, prefix="/api/v1", tags=["notes"])
