1a3b7253f134a8fad395a2ddc766f68c626bf1cd
John Kristan Torremocha <johnkristan01@gmail.com>
2025-07-18 22:56:35 +0800

    entrypoint execution code

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+echo "Running Alembic migrations..."
+alembic upgrade head
+
+echo "Migrations completed. Starting FastAPI application..."
+exec uvicorn main:app --host 0.0.0.0 --port 8000 --lifespan on 
